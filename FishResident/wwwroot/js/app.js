e7fe9076716f23b33ed9d73d0cb4a33edfca4e60
Anya Ostanina <oav@paraweb.me>
2019-06-25 14:46:21 +0700

    add map

--- FishResident/wwwroot/js/app.js
+++ FishResident/wwwroot/js/app.js
@@ -282,6 +282,67 @@ $(function () {
     new _tab2.default();
 
     new _field2.default('.js-field');
+
+    var uploadButton = $('.browse-btn');
+    var fileInfo = $('.file-info');
+    var realInput = $('real-input');
+
+    uploadButton.on('click', function () {
+        realInput.click();
+    });
+
+    realInput.on('change', function () {
+        var name = realInput.value.split(/\\|\//).pop();
+        var truncated = name.length > 20 ? name.substr(name.length - 20) : name;
+
+        fileInfo.innerHTML = truncated;
+    });
+
+    $('.sel').each(function () {
+        $(this).children('select').css('display', 'none');
+
+        var $current = $(this);
+
+        $(this).find('option').each(function (i) {
+            if (i == 0) {
+                $current.prepend($('<div>', {
+                    class: $current.attr('class').replace(/sel/g, 'sel__box')
+                }));
+
+                var placeholder = $(this).text();
+                $current.prepend($('<span>', {
+                    class: $current.attr('class').replace(/sel/g, 'sel__placeholder'),
+                    text: placeholder,
+                    'data-placeholder': placeholder
+                }));
+
+                return;
+            }
+
+            $current.children('div').append($('<span>', {
+                class: $current.attr('class').replace(/sel/g, 'sel__box__options'),
+                text: $(this).text()
+            }));
+        });
+    });
+
+    // Toggling the `.active` state on the `.sel`.
+    $('.sel').click(function () {
+        $(this).toggleClass('active');
+    });
+
+    // Toggling the `.selected` state on the options.
+    $('.sel__box__options').click(function () {
+        var txt = $(this).text();
+        var index = $(this).index();
+
+        $(this).siblings('.sel__box__options').removeClass('selected');
+        $(this).addClass('selected');
+
+        var $currentSel = $(this).closest('.sel');
+        $currentSel.children('.sel__placeholder').text(txt);
+        $currentSel.children('select').prop('selectedIndex', index + 1);
+    });
 }); // check responsive
 // modernizr (csspointerevents objectfit touchevents)
 // import { throttle, debounce } from 'throttle-debounce'; // github.com/niksy/throttle-debounce
